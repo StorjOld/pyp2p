f590820802dea1b5ad7b3c57ee8e86203909f3bb
Matthew Roberts <matthew@roberts.pm>
2015-11-05 17:19:46 +1100

    Make module installable from pypi. Add module to pypi.

--- setup.py
+++ setup.py
@@ -1,21 +1,43 @@
 from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
 
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    version='0.1',
-    description='An open source, Python 2/3, simple p2p library',
-    url='http://github.com/robertsdotpm/pyp2p',
+    version='0.0.1',
+    name='pyp2p',
+    description='Python P2P networking library',
+    ketwords='NAT traversal, TCP hole punching, simultaneous open, UPnP, NATPMP, P2P, Peer-to-peer networking library, python',
+    long_description=long_description,
+    url='http://github.com/Storj/pyp2p',
     author='Matthew Roberts',
     author_email='matthew@roberts.pm',
     license='MIT',
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
-        'netifaces',
-        'ntplib',
-        'twisted',
-        'ipaddress',
-        'requests'
-    ]
+        'netifaces>=0.10.4',
+        'ntplib>=0.3.3',
+        'twisted>=15.4.0',
+        'ipaddress>=1.0.14',
+        'requests>=2.8.1'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
 )
+
 
 
