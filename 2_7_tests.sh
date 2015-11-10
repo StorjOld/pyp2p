09e262e24091613d91b7d01f066479717ba7d2df
Matthew Roberts <matthew@roberts.pm>
2015-11-10 21:40:58 +1100

    Attempt to make tests run on travis.

--- 2_7_tests.sh
+++ 2_7_tests.sh
@@ -1,15 +1,4 @@
 cd tests
-echo "Testing with Python 3."
-python3.3 -m "nose" -v test_dht_msg.py
-python3.3 -m "nose" -v test_nat_pmp.py
-python3.3 -m "nose" -v test_net.py
-python3.3 -m "nose" -v test_lib.py
-python3.3 -m "nose" -v test_rendezvous_client.py
-python3.3 -m "nose" -v test_rendezvous_server.py
-python3.3 -m "nose" -v test_sock.py
-python3.3 -m "nose" -v test_unl.py
-python3.3 -m "nose" -v test_upnp.py
-
 echo "Testing with Python 2.7."
 python2.7 -m "nose" -v test_dht_msg.py
 python2.7 -m "nose" -v test_nat_pmp.py
